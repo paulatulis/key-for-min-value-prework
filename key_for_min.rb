41f63ff4cadc3cef06270b12a2e9168be8e4d98c
Paula Tulis <tulis.paula@gmail.com>
2019-03-19 11:38:46 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,11 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
+name_hash = {"dad" => 59, "mom" => 57, "brother" => 24}
 
 def key_for_min_value(name_hash)
-
+  key = ""
+name_hash.collect do |name, age|
+  name <=> age
+  key = name 
 end
+end 
