a56d155e87f82539f7439c75d207ad21c4f26586
Paula Tulis <tulis.paula@gmail.com>
2019-03-19 11:52:33 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,7 +8,7 @@ def key_for_min_value(name_hash)
   
 name_hash.each do |name, age|
   if empty_value == nil || age < empty_value
-  empty_key == 
+  empty_key = name 
   name <=> age
   key = name 
 end
